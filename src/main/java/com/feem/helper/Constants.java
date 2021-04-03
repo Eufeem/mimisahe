4dc106730aaf14713c8fea412c8f50e26c75ab8e
Eufeem <eufeemgr@gmail.com>
2021-04-03 16:55:55 -0600

    Constants

--- src/main/java/com/feem/helper/Constants.java
+++ src/main/java/com/feem/helper/Constants.java
@@ -13,21 +13,20 @@ public class Constants {
 	
 	// DataBase
 	public static final String DATA_BASE = "mimisahe";
-	
 	public static final String SQL_SELECT = " SELECT ";
 	public static final String SQL_FROM = " FROM ";
 	public static final String SQL_WHERE = " WHERE ";
 	public static final String SQL_WHERE_STATUS = " WHERE STATUS = 1 ";
 	public static final String SQL_ORDER_BY_1 = " ORDER BY 1 ";
 	public static final String SQL_ORDER_BY_CODE = " ORDER BY CODE ";
-	
 	public static final String SQL_INSERT_INTO = " INSERT INTO ";
 	public static final String SQL_VALUES = " VALUES ";
 	public static final String SQL_UPDATE = " UPDATE ";
 	public static final String SQL_SET = " SET ";
 	public static final String SQL_DELETE_FROM = " DELETE FROM ";
-	
 	public static final String TABLE_USER = " T_USER ";
 	public static final String TABLE_ROLE = " T_ROLE ";
 	public static final String TABLE_FUNCTION = " T_FUNCTION ";
+	
+	
 }
